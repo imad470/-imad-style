f0fdc36d19c12bdea22a84cf1ddffce48e9667ab
Hachouch <imadff73784@icloud.com>
2025-06-24 22:47:51 +0200

    Create create-checkout-session.js

---
+++ netlify/functions/create-checkout-session.js
@@ -0,0 +1,37 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async function(event, context) {
+  try {
+    const { items } = JSON.parse(event.body);
+
+    const lineItems = items.map(item => ({
+      price_data: {
+        currency: 'eur',
+        product_data: {
+          name: item.name,
+          images: [item.image],
+        },
+        unit_amount: item.price * 100, // prix en centimes
+      },
+      quantity: item.quantity,
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card', 'paypal'],
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `${process.env.URL}/success.html`,
+      cancel_url: `${process.env.URL}/cancel.html`,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ id: session.id }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
